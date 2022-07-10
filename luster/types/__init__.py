255bd6f22b833ad7efacab96e95e7fc027bbd870
Izhar Ahmad <thisisizharahmad@gmail.com>
2022-07-09 20:50:37 -0700

    Add API types for users

--- luster/types/__init__.py
+++ luster/types/__init__.py
@@ -5,5 +5,8 @@ luster.types
 Simple types for Revolt API.
 """
 
+from luster.types.enums import *
+from luster.types.file import *
 from luster.types.node_info import *
+from luster.types.users import *
 from luster.types.websocket import *
---
+++ luster/types/file.py
@@ -0,0 +1,62 @@
+# Copyright (C) I. Ahmad (nerdguyahmad) 2022-2023
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional, TypedDict
+from typing_extensions import NotRequired
+
+if TYPE_CHECKING:
+    from luster.types.enums import FileType
+
+
+__all__ = (
+    "FileMetadata",
+    "File",
+)
+
+
+class FileMetadata(TypedDict):
+    """Represents meta data for a :class:`File`."""
+
+    type: FileType
+    """The type of file."""
+
+
+class File(TypedDict):
+    """Represents a file asset from Revolt file server."""
+
+    _id: str
+    """The unique ID of file."""
+
+    tag: str
+    """The tag or bucket that the file was uploaded to."""
+
+    filename: str
+    """The name of file."""
+
+    metadata: FileMetadata
+    """The extra metadata associated to this file."""
+
+    content_type: str
+    """The content type of this file."""
+
+    size: int
+    """The size of file in bytes."""
+
+    deleted: NotRequired[Optional[bool]]
+    """Whether this file was deleted."""
+
+    reported: NotRequired[Optional[bool]]
+    """Whether this file was reported."""
+
+    message_id: NotRequired[Optional[str]]
+    """The ID of message that the file was uploaded to."""
+
+    user_id: NotRequired[Optional[str]]
+    """The ID of user that uploaded this file."""
+
+    server_id: NotRequired[Optional[str]]
+    """The ID of server that the file was uploaded to."""
+
+    object_id: NotRequired[Optional[str]]
+    """The ID of object associated to this file such as a user."""
